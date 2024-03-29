9c2fe5d76d4b8896947e5a2a9c373b66e3b7dc9e
shaon1087 <shaonsaha1087@gmail.com>
2024-03-30 03:03:46 +0600

    save localStorage function added

--- handler.js
+++ handler.js
@@ -1,25 +1,35 @@
-const addProduct = ()=>{
-    const itemField = document.getElementById("Item-input");
-    const quantityField = document.getElementById("Quantity-input");
-    const item = itemField.value;
-    const quantity = quantityField.value;
-      itemField.value = "";
-      quantityField.value = "";
+const addProduct = () => {
+  const itemField = document.getElementById("Item-input");
+  const quantityField = document.getElementById("Quantity-input");
 
-    const outputField = document.getElementById("output");
-    console.log(item, quantity);
-  
-    displayProduct(item, quantity);
+  const item = itemField.value;
+  const quantity = quantityField.value;
 
-    
+  itemField.value = "";
+  quantityField.value = "";
 
-}
+  displayProduct(item, quantity);
+  saveShoppingCart(item, quantity);
+};
 
-const displayProduct = (item,quantity)=>{
-      const outputField = document.getElementById("output");
-         const li = document.createElement("li");
-         li.innerText = `${item} ${quantity}`;
+const displayProduct = (item, quantity) => {
+  const outputField = document.getElementById("output");
+  const li = document.createElement("li");
+  li.innerText = `${item} ${quantity}`;
+  outputField.appendChild(li);
+};
 
-         outputField.appendChild(li);
-}
+const getStoredShoppingCart = () => {
+    let cart = {};
+    const storedCart =  localStorage.getItem("cart");
+    if (storedCart){
+        cart = JSON.parse(storedCart);
+    }
+    return cart;
+};
 
+const saveShoppingCart = (item, quantity) => {
+  const cart = getStoredShoppingCart();
+  cart[item] = quantity;
+  console.log(cart);
+};
